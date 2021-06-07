dc55af05c4ae9379448d290bd965e6d162adc31a
Khoa Tran Minh <ktm8@posteo.de>
2021-06-07 21:47:58 +0200

    Add Radix-2 FFT and license information
    - Add fft_fw_rec() and fft_bw_rec(). They are sequential Radix-2
    algorithm, ported from C to C++ style. Names are likely going to change
    in the future.
    - Use GNU GPLv3 for all the code

--- lib.cc
+++ lib.cc
@@ -1,6 +1,83 @@
-#include <iostream>
+/*
+   Copyright (C) 2021 Minh Khoa Tran
 
-void hello(void)
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   */
+
+#include <cmath>
+#include <complex>
+#include <iostream>
+#include <vector>
+
+void fft_fw_rec(std::vector<std::complex<double>> &v, std::size_t n)
 {
-	std::cout << "Hello, World" << std::endl;
+	std::size_t N, k;
+	std::vector<std::complex<double>> v0, v1;
+
+	N = 1 << n;
+	if (N == 1)
+		return;
+
+	v0.resize(N / 2);
+	v1.resize(N / 2);
+	for (k = 0; k < N / 2; k++) {
+		v0[k] = v[2 * k];
+		v1[k] = v[2 * k + 1];
+	}
+
+	fft_fw_rec(v0, n-1);
+	fft_fw_rec(v1, n-1);
+
+	for (k = 0; k < N / 2; k++) {
+		std::complex<double> tw;
+
+		tw = std::polar(1., -2. * acos(-1) * k / N);
+
+		v[k]         = v0[k] + tw * v1[k];
+		v[k + N / 2] = v0[k] - tw * v1[k];
+	}
+
+	return;
+}
+
+void fft_bw_rec(std::vector<std::complex<double>> &v, std::size_t n)
+{
+	std::size_t N, k;
+	std::vector<std::complex<double>> v0, v1;
+
+	N = 1 << n;
+	if (N == 1)
+		return;
+
+	v0.resize(N / 2);
+	v1.resize(N / 2);
+	for (k = 0; k < N / 2; k++) {
+		v0[k] = v[2 * k];
+		v1[k] = v[2 * k + 1];
+	}
+
+	fft_bw_rec(v0, n-1);
+	fft_bw_rec(v1, n-1);
+
+	for (k = 0; k < N / 2; k++) {
+		std::complex<double> tw;
+
+		tw = std::polar(1., 2. * acos(-1) * k / N);
+
+		v[k]         = (v0[k] + tw * v1[k]) / 2.;
+		v[k + N / 2] = (v0[k] - tw * v1[k]) / 2.;
+	}
+
+	return;
 }
--- noq.cc
+++ noq.cc
@@ -1,6 +1,56 @@
+/*
+   Copyright (C) 2021 Minh Khoa Tran
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   */
+
+#include <complex>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
 #include "lib.h"
 
 int main(void)
 {
-	hello();
+	std::vector<std::complex<double>> v = {
+		std::complex<double> (1., 2.),
+		std::complex<double> (3., 4.),
+		std::complex<double> (5., 6.),
+		std::complex<double> (7., 8.),
+	};
+
+	std::cout << std::fixed << std::setprecision(1);
+
+	std::cout << "Initial" << std::endl;
+	for (std::complex<double> e: v) {
+		std::cout << e << std::endl;
+	}
+
+	fft_fw_rec(v, 2);
+
+	std::cout << "Forward" << std::endl;
+	for (std::complex<double> e: v) {
+		std::cout << e << std::endl;
+	}
+
+	fft_bw_rec(v, 2);
+
+	std::cout << "Backward" << std::endl;
+	for (std::complex<double> e: v) {
+		std::cout << e << std::endl;
+	}
+
+	return 0;
 }
